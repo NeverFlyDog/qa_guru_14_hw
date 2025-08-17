658b08a507a7f6c57cb3d5d1836d6c889f7c3199
NeverFlyDog <217641660+NeverFlyDog@users.noreply.github.com>
2025-08-17 14:28:25 +0300

    Update selenoid url

--- src/test/java/com/kinorium/ru/utils/AllureAttachments.java
+++ src/test/java/com/kinorium/ru/utils/AllureAttachments.java
@@ -66,7 +66,7 @@ public class AllureAttachments {
     }
 
     private static URL getVideoUrl() {
-        String videoUrl = "https://selenoid.autotests.cloud/video/" + Selenide.sessionId() + ".mp4";
+        String videoUrl = "https://ru.selenoid.autotests.cloud/video/" + Selenide.sessionId() + ".mp4";
         try {
             return URI.create(videoUrl).toURL();
         } catch (MalformedURLException e) {
